c539f964539bec4a7da29d35744f3b26b152b6c6
Thomas Tsuma <34827918+TomiTsuma@users.noreply.github.com>
2025-11-09 17:33:20 +0300

    Navidrome installer

--- homelab/media/navidrome/docker-install.sh
+++ homelab/media/navidrome/docker-install.sh
@@ -1,6 +1,1 @@
-docker run -d \
-  --name=navidrome \
-  -v /mnt/music:/music \
-  -v /mnt/data:/data \
-  -p 4533:4533 \
-  deluan/navidrome:latest
+docker run -d --name=navidrome -v /mnt/music:/music -v /mnt/data:/data -p 4533:4533 deluan/navidrome:latest
