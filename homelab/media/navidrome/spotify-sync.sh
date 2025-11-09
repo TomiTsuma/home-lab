79779f780e3e395945dc12bb1b4be9d32c738271
Thomas Tsuma <34827918+TomiTsuma@users.noreply.github.com>
2025-11-09 18:12:14 +0300

    spotify sync

---
+++ homelab/media/navidrome/spotify-sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3.11 -m pip install spotDL
+
+python3.11 -m spotdl --download-ffmpeg
+
+python3.11 -m spotdl download https://open.spotify.com/playlist/77lCtD1ewN3KxWLg4AkJfQ
+
+python3.11 -m spotdl download https://open.spotify.com/playlist/7aIc59Tt6c56iV5gwRR0YJ
