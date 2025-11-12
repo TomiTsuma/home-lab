77f73026f9db436177d68ef6dec773c16bcf5655
Thomas Tsuma <34827918+TomiTsuma@users.noreply.github.com>
2025-11-12 17:52:37 +0300

    Nvidia container toolkit installation

---
+++ homelab/compute/nvidia/nvidia-container-toolkit-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+sudo gpg --keyserver keyserver.ubuntu.com --recv-keys DDCAE044F796ECB0
+
+sudo gpg --export --armor DDCAE044F796ECB0 | sudo tee /etc/apt/trusted.gpg.d/nvidia-toolkit.asc
+
+sudo apt update
+
+sudo apt install nvidia-container-toolkit
