4c8bd39a19bd199915242a214efcdb7ca6573d1b
Thomas Tsuma <34827918+TomiTsuma@users.noreply.github.com>
2025-12-05 13:31:14 +0300

    Docker nct enabled

--- homelab/compute/nvidia/nvidia-container-toolkit-install.sh
+++ homelab/compute/nvidia/nvidia-container-toolkit-install.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
 
-sudo gpg --keyserver keyserver.ubuntu.com --recv-keys DDCAE044F796ECB0
+# sudo gpg --keyserver keyserver.ubuntu.com --recv-keys DDCAE044F796ECB0
 
-sudo gpg --export --armor DDCAE044F796ECB0 | sudo tee /etc/apt/trusted.gpg.d/nvidia-toolkit.asc
+# sudo gpg --export --armor DDCAE044F796ECB0 | sudo tee /etc/apt/trusted.gpg.d/nvidia-toolkit.asc
 
-sudo apt update
+# sudo apt update
 
-sudo apt install nvidia-container-toolkit
+# sudo apt install nvidia-container-toolkit
+
+# sudo nvidia-ctk runtime configure --runtime=docker
+# sudo systemctl restart docker
+
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+   curl \
+   gnupg2
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+
+sudo apt-get install -y nvidia-container-toolkit
 
 sudo nvidia-ctk runtime configure --runtime=docker
+
 sudo systemctl restart docker
