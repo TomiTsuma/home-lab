319edd0a50960601e1379ee8fe2988bded2b8ea5
Thomas Tsuma <34827918+TomiTsuma@users.noreply.github.com>
2026-08-01 16:47:57 +0300

    Added portainer agent

---
+++ homelab/monitoring/portainer/start_agent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+docker run -d \
+  --name portainer_agent \
+  --restart=always \
+  -p 9001:9001 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  portainer/agent:latest
